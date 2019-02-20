c3806b37899274bb0e48696084cd1822bf7bd1b2
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2019-02-20 15:47:19 +0530

    added MVVC pattern

--- URLMaster/ViewController.swift
+++ URLMaster/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-import Alamofire
+
 
 class ViewController: UIViewController {
+    
+    var viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,106 +21,39 @@ class ViewController: UIViewController {
     
     @IBAction func GetAction(_ sender: Any) {
         
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        viewModel.getActionMethod()
         
-        let session = URLSession.shared
-        session.dataTask(with: url) { (data, response, error) in
-            if let response = response {
-                print("response = \(response)")
-            }
-            
-            if let datas = data {
-                
-                do{
-                    let json = try JSONSerialization.jsonObject(with: datas, options: [])
-                    print("json = \(json)")
-                }catch{
-                    print("Error= \(error.localizedDescription)")
-                }
-            }
-            
-           
-     
-        }.resume()
+
         
     }
     
+    
+    
     @IBAction func postAction(_ sender: Any) {
         
-        let parameters = ["username":"AnupTechTonic","tweets":"HELLOOO"]
-        
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
-        
-        request.httpBody = httpBody
-        
-         let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print("ResponsPost = \(response)")
-            }
-            
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print("JSON = \(json)")
-                } catch {
-                    print("Error = \(error.localizedDescription)")
-                }
-            }
-        }.resume()
+       viewModel.postActionMethod()
         
     }
+    
+    //MARK:
+    
     
     
     @IBAction func AlamofireGet(_ sender: Any) {
         
-//         guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
-        
-        let url = "https://jsonplaceholder.typicode.com/users"
-        
-        Alamofire.request(url, method: .get, parameters: ["":""]).validate().responseJSON { (response) in
-            print("response = \(response)")
-            
-            switch response.result{
-            case .success(let data):
-                let json = data as AnyObject
-                print("JSON = \(json)")
-            case .failure(let error):
-                print("Error = \(error.localizedDescription)")
-            }
-            
-            
-        }
-        
+        viewModel.AlamofireGet()
+
     }
+    
     
     
     @IBAction func AlamofirePost(_ sender: Any) {
         
-        let params = ["username":"AnupTechTonic","tweets":"HELLOOO"]
-        let url = "https://jsonplaceholder.typicode.com/posts"
-        
-        Alamofire.request(url, method: .post, parameters: params).validate().responseJSON { (response) in
-            print("response = \(response)")
-            
-            switch response.result{
-            case .success(let data):
-                let json = data as AnyObject
-                print("JSON = \(json)")
-            case .failure(let error):
-                print("Error = \(error.localizedDescription)")
-            }
-        }
-        
-        
-        
+        viewModel.AlamofirePost()
         
     }
+    
+   
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
